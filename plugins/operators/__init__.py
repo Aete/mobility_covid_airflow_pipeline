43a75d7cfd0a54e5835104322ac90564564cb61b
Aete <hsg8234@naver.com>
2022-04-05 21:16:42 +0900

    :construction: working with seoul_living_migration
    add an operator 'LoadSeoulMigation' to __init__.py

--- plugins/operators/__init__.py
+++ plugins/operators/__init__.py
@@ -1,9 +1,11 @@
 from operators.load_case import LoadCaseOperator
 from operators.load_vaccination import LoadVaccinationOperator
 from operators.load_apple_index import LoadAppleIndex
+from operators.load_seoul_living_migration import LoadSeoulMigration
 
 __all__ = [
     'LoadCaseOperator',
     'LoadVaccinationOperator',
-    'LoadAppleIndex'
+    'LoadAppleIndex',
+    'LoadSeoulMigration'
 ]
